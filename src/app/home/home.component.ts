58005fa068bc24c193c35930e3d5d228f157935a
fabricionetto <agostinhonetto73@gmail.com>
2024-05-19 15:07:38 -0300

    Quinto Commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardsCasasComponent } from '../cards-casas/cards-casas.component';
 import { ContratoCasas } from '../contrato-casas';
 import { CommonModule } from '@angular/common';
+import { LocalCasasService } from '../local-casas.service';
 
 @Component({
   selector: 'app-home',
@@ -12,106 +13,10 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
-  listaContratoCasas: ContratoCasas [] =  [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `/assets/12.jpg`,
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: `/assets/12.jpg`,
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: `/assets/12.jpg`,
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: `/assets/12.jpg`,
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: `/assets/12.jpg`,
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `/assets/12.jpg`,
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `/assets/12.jpg`,
-      availableUnits: 5,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `/assets/12.jpg`,
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: `/assets/12.jpg`,
-      availableUnits: 10,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: `/assets/12.jpg`,
-      availableUnits: 6,
-      wifi: true,
-      laundry: true,
-    },
-  ];
+  casaServico: LocalCasasService = inject(LocalCasasService);
+  listaContratoCasas: ContratoCasas []| undefined;
+
+  constructor(){
+    this.listaContratoCasas = this.casaServico.buscarTodasCasas();
+  }
 }
