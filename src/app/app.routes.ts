e8671ed1b5f33fc9dbe9ec9e23a25bec811aecdc
fabricionetto <agostinhonetto73@gmail.com>
2024-05-19 17:04:02 -0300

    Rotas-Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CardsCasasDetalhesComponent } from './cards-casas-detalhes/cards-casas-detalhes.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path:'', component: HomeComponent, title: 'Home Page'},
+    {path:'detalhes/:id', component: CardsCasasDetalhesComponent, title: 'Detalhes cartoes'},
+];
