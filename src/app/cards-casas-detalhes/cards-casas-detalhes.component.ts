47588cebd6f0be81cdffb57c6cb4e6c57a7c8322
fabricionetto <agostinhonetto73@gmail.com>
2024-05-19 22:20:46 -0300

    Detalhes Casa

--- src/app/cards-casas-detalhes/cards-casas-detalhes.component.ts
+++ src/app/cards-casas-detalhes/cards-casas-detalhes.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LocalCasasService } from '../local-casas.service';
+import { ContratoCasas } from '../contrato-casas';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cards-casas-detalhes',
@@ -8,5 +11,13 @@ import { Component } from '@angular/core';
   styleUrl: './cards-casas-detalhes.component.css'
 })
 export class CardsCasasDetalhesComponent {
+ 
+  rota: ActivatedRoute = inject(ActivatedRoute);
+  casaServico: LocalCasasService = inject(LocalCasasService);
+  casasLocal: ContratoCasas | undefined;
 
+  constructor(){
+    const numID = Number(this.rota.snapshot.params['id']);
+    this.casasLocal = this.casaServico.buscarPorID(numID);
+  }
 }
--- src/app/cards-casas/cards-casas.component.ts
+++ src/app/cards-casas/cards-casas.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ContratoCasas } from '../contrato-casas';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cards-casas',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cards-casas.component.html',
   styleUrl: './cards-casas.component.css'
 })
